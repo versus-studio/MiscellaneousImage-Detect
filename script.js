52b87eb870fbb02589d6a3174f8d5733e3116f65
vipul-dalal <rudradalal0082004@gmail.com>
2024-01-17 16:48:00 +0530

    update

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ window.onscroll = () => {
     }
   });
 };
+
+function uploadLinks() {
+    document.getElementById('uploadPopup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('uploadPopup').style.display = 'none';
+}
+
+function displayImage() {
+    const input = document.getElementById('imageInput');
+    const image = document.getElementById('selectedImage');
+
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            image.src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
